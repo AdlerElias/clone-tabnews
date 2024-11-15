32610a72632b7e0dbfcf60d20700d0d5e14a282b
Adler Elias <adlerge99@gmail.com>
2024-11-15 14:19:42 +0000

    adiciona prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Mari, eu amo você, Se vc me ama da uma risadinha!</h1>
+function Home() {
+  return <h1>Mari, eu amo você, Se vc me ama da uma risadinha!</h1>;
+}
+
+function teste() {
+  return teste < h2 > <h2 />;
 }
 
 export default Home;
